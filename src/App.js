2f05da1f3071acac90dae131e4c1bddc8a07d206
academyitwill <academyitwill@gmail.com>
2025-10-17 17:02:48 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const MOVIE__URL = "https://yts.mx/api/v2/list_movies.json";
+  const MOVIE__URL = "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year";
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   useEffect(() => {
@@ -11,13 +11,27 @@ function App() {
       .then((json) => {
         setMovies(json.data.movies);
         setLoading(false);
+        console.log(json.data.movies);
       });
   }, []);
   return (
     <div >
-      {loading ? 
+      {loading ?
         <h1>Loading...</h1> :
-        movies.map((movie)=><div key={movie.id}>{movie.title}</div>)
+
+        movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}/>
+              <a href={movie.url}><h2>{movie.title}</h2></a>
+              <p>{movie.summary}</p>
+              <ul>
+                 {movie.genres.map((g)=>(
+                    <li key={g}>{g}</li>
+                 ))} 
+              </ul>
+            </div>
+          )
+        )
       }
     </div>
 
