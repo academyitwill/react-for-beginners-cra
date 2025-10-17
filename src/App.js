62aa03af1bbd9f035ebb831589ff990f1cf07f9a
academyitwill <academyitwill@gmail.com>
2025-10-17 11:57:35 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo,setTodo]=useState("");
-  const [toDos,setTodos]=useState([]);
-  const onChange=(event)=>setTodo(event.target.value);
-  const onSubmit=(event)=>{
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
     event.preventDefault();
-    if(toDo===""){
+    if (toDo === "") {
       return;
     }
+    setTodos(currentArray => [toDo, ...currentArray]);
     setTodo("");
-    setTodos(currentArray=>[toDo,...currentArray])
-    console.log(toDos);
-
   };
   return (
     <div >
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
-        <input  onChange={onChange} 
-                type="text" 
-                value={toDo} 
-                placeholder="Write your to do..."/>
-
+        <input onChange={onChange}
+          type="text"
+          value={toDo}
+          placeholder="Write your to do..." />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item,index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
