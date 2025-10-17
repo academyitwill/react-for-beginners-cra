57da4b8fb3bff50580a0013f462e80f227f15be6
academyitwill <academyitwill@gmail.com>
2025-10-17 15:03:09 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const COIN_PAPRIKA_URL="https://api.coinpaprika.com/v1/tickers";
+  const MOVIE__URL = "https://yts.mx/api/v2/list_movies.json";
   const [loading, setLoading] = useState(true);
-  const [coins,setCoins]=useState([]);
-  useEffect(()=>{
-    fetch(COIN_PAPRIKA_URL)
-      .then((response)=>response.json())
-      .then((json)=>{
-        setCoins(json);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(MOVIE__URL)
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        console.log(json.data.movies);
         setLoading(false);
       });
-  },[]);
+  }, []);
   return (
     <div >
-      <h1>The Coins!</h1>
-      {loading?<strong>Loading...</strong>:null}
+      <h1>The Coins!{loading ? "" : `(${movies.length})`}</h1>
+      {loading ? <strong>Loading...</strong> :
+       
+       movies.map()
+       
+       }
     </div>
+
   );
 }
 
